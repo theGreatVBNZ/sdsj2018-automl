512998c569754966fbb6cd7a081a75ccf49c1141
Dmitry Savostyanov <ds@gosu.ai>
2018-10-04 02:50:11 +0300

    Features: Lag and Aggregate, TODO: one_hot_encoding fix

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import time
 from functools import lru_cache, partial
 from itertools import product
 from IPython.display import display
@@ -12,7 +13,7 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 
 
-# @lru_cache()
+@lru_cache()
 def load(task, table, **kwargs):
     """
     :param task: Номер задачи
@@ -56,14 +57,16 @@ def pprint(*args):
 
 
 def parse_dt(x):
-    if not isinstance(x, str):
-        return None
+    if isinstance(x, datetime.datetime) or isinstance(x, datetime.date):
+        return x
+    elif not isinstance(x, str):
+        return np.nan
     elif len(x) == len('2010-01-01'):
         return datetime.datetime.strptime(x, '%Y-%m-%d')
     elif len(x) == len('2010-01-01 10:10:10'):
         return datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
     else:
-        return None
+        return np.nan
     
 
 def select_datetime_columns(df):
@@ -123,6 +126,26 @@ def one_hot_encoding(df_x, categorical):
     return pd.concat([df_x, df_dummies], axis=1)
 
 
+def loading_bar(idx, total, value, frequency=1):
+    if idx % frequency == 0:
+        pprint(f'[{idx + 1} / {total}]: {value}')
+
+
+def one_hot_encoding_fast(df_x, categorical):
+    dummies = [df_x, ]
+    for i, (col_name, unique_values) in enumerate(categorical.items()):
+        loading_bar(i, len(categorical), col_name, frequency=500)
+        if col_name in df_x.columns:
+            df_dummies = pd.get_dummies(df_x[col_name], prefix=f'onehot_{col_name}', prefix_sep='=')
+            dummies.append(df_dummies)
+        else:
+            for unique_value in unique_values:
+                df_dummies = pd.DataFrame()
+                df_dummies[f'onehot_{col_name}={unique_value}'] = np.nan
+                dummies.append(df_dummies)
+    return pd.concat(dummies, axis=1)
+
+
 def find_missings(df_x):
     columns_with_missings = df_x.columns[df_x.isna().any()].tolist()
     return columns_with_missings
@@ -136,6 +159,12 @@ def fill_missings(df_x, columns):
 
 
 def select_numeric_columns(df_x):
+    # for col in df_x.columns:
+    #     try:
+    #         col.startswith('number')
+    #     except Exception:
+    #         print(col, df_x[col])
+
     numeric_columns = [
         col_name
         for col_name in df_x.columns
@@ -158,6 +187,48 @@ def scale(df_x, scaler):
     return pd.DataFrame(data=scaler.transform(df_x), columns=df_x.columns)
 
 
+def shift_by_datetime(df_x, dt_column_name):
+    df_shifted = df_x.sort_values(dt_column_name).shift(-1)
+    df_shifted.columns = [f'{col}__{dt_column_name}__1' for col in df_shifted.columns]
+    return df_shifted
+
+
+def create_lag_features(df_x, datetime_columns):
+    tables = [df_x, ]
+    for dt_column in datetime_columns:
+        df_shifted = shift_by_datetime(df_x, dt_column)
+        tables.append(df_shifted)
+    return pd.concat(tables, axis=1)
+
+
+def aggregate(df_x, by, aggregations=('sum', 'count', 'std', 'min', 'max')):
+    df_aggregated = df_x.groupby(by).agg(aggregations)
+    df_aggregated.columns = [f'{col[0]}_aggby_{by}_{col[1]}' for col in df_aggregated.columns.values]
+    return df_aggregated
+
+
+def merge_with_aggregated(df_x, df_aggregated, on):
+    return df_x.merge(df_aggregated, left_on=on, right_index=True, how='left')
+
+
+def select_aggregation_columns(dt_features):
+    selected_for_aggregation = []
+    valid_prefixes = ('dt_number_day', 'dt_number_weekday', 'dt_number_month')
+    for feature in dt_features:
+        for valid_prefix in valid_prefixes:
+            if feature.startswith(valid_prefix):
+                selected_for_aggregation.append(feature)
+    return selected_for_aggregation
+
+
+def create_aggregations(df_x, by):
+    for i, feature in enumerate(by):
+        loading_bar(i, len(by), feature)
+        df_aggregated = aggregate(df_x, feature)
+        df_x = merge_with_aggregated(df_x, df_aggregated, feature)
+    return df_x
+
+
 def make_predictions(df_transformed, model, proba=False):
     if proba:
         predictions = model.predict_proba(df_transformed)[:, 1]
@@ -170,29 +241,65 @@ def transform_data(df_x, target):
     # TODO: inplace functions
     pipeline = []
 
+    # DateTime features
     datetime_columns = select_datetime_columns(df_x)
+    start_columns = list(df_x.columns)
     df_x = transform_datetime_features(df_x, datetime_columns)
+    dt_features = [col for col in df_x.columns if col not in start_columns]
     pipeline.append(partial(transform_datetime_features, datetime_columns=datetime_columns))
-    
+
+    # Lag Features
+    start_time = time.time()
+    start_columns = df_x.columns
+    df_x = create_lag_features(df_x, datetime_columns)
+    pipeline.append(partial(create_lag_features, datetime_columns=datetime_columns))
+    elapsed_time = time.time() - start_time
+    new_columns = [col for col in df_x if col not in start_columns]
+    pprint(f'Time elapsed for {create_lag_features}: {elapsed_time} \nColumns updated: {len(new_columns)}')
+
+    # Aggregations
+    start_time = time.time()
+    start_columns = df_x.columns
+    selected_for_aggregation = select_aggregation_columns(dt_features)
+    df_x = create_aggregations(df_x, by=selected_for_aggregation)
+    pipeline.append(partial(create_aggregations, by=selected_for_aggregation))
+    new_columns = [col for col in df_x if col not in start_columns]
+    pprint(f'Time elapsed for {create_aggregations}: {time.time() - start_time} \nColumns updated: {len(new_columns)}')
+
+    # Constant Features
+    start_time = time.time()
     constant_columns = constant_features(df_x)
     df_x = drop_columns(df_x, cols=constant_columns)
     pipeline.append(partial(drop_columns, cols=constant_columns))
+    pprint(f'Time elapsed for {drop_columns}: {time.time() - start_time} \nColumns updated: {len(constant_columns)}')
 
+    # Categorical Features
+    start_time = time.time()
     categorical = select_for_encoding(df_x, max_unique=20)
-    df_x = one_hot_encoding(df_x, categorical=categorical)
-    pipeline.append(partial(one_hot_encoding, categorical=categorical))
+    df_x = one_hot_encoding_fast(df_x, categorical=categorical)
+    pipeline.append(partial(one_hot_encoding_fast, categorical=categorical))
+    pprint(f'Time elapsed for {one_hot_encoding_fast}: {time.time() - start_time} \nColumns updated: {len(categorical)}')
 
+    # Missings
+    start_time = time.time()
     columns_with_missings = find_missings(df_x)
     df_x = fill_missings(df_x, columns=columns_with_missings)
     pipeline.append(partial(fill_missings, columns=columns_with_missings))
+    pprint(f'Time elapsed for {fill_missings}: {time.time() - start_time} \nColumns updated: {len(columns_with_missings)}')
 
+    # Keep Numeric
+    start_time = time.time()
     numeric_columns = select_numeric_columns(df_x)
     df_x = keep_columns(df_x, columns=numeric_columns)
     pipeline.append(partial(keep_columns, columns=numeric_columns))
+    pprint(f'Time elapsed for {keep_columns}: {time.time() - start_time} \nColumns updated: {len(numeric_columns)}')
 
+    # Scale
+    start_time = time.time()
     scaler = create_scaler(df_x)
     df_x = scale(df_x, scaler)
     pipeline.append(partial(scale, scaler=scaler))
+    pprint(f'Time elapsed for {scale}: {time.time() - start_time} \nColumns updated: {len(numeric_columns)}')
 
     df_transformed = df_x
     return pipeline, df_transformed
